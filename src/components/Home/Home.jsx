8d99ef0472573dd650b996081e0498f33345f6c7
Akash Vaidya <akash.vaidya@arraypointer.com>
2024-11-18 11:35:18 +0530

    some css changes.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,15 +5,19 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <h4 className={style.heading}>I AM A</h4>
       <div className={style.home}>
-        <Button as={Link} to={"student/login"} variant="outline-primary">
-          Student
-        </Button>
-        <Button as={Link} to={"teacher/login"} variant="outline-primary">
-          Teacher
-        </Button>
-        <Button as={Link} to={"admin/login"} variant="outline-primary">Admin</Button>
+        <h4 className={style.heading}>I AM A</h4>
+        <div className={style.buttonRow}>
+          <Button as={Link} to={"student/login"} variant="outline-primary">
+            Student
+          </Button>
+          <Button as={Link} to={"teacher/login"} variant="outline-primary">
+            Teacher
+          </Button>
+          <Button as={Link} to={"admin/login"} variant="outline-primary">
+            Admin
+          </Button>
+        </div>
       </div>
     </>
   );
